0a41b78fc4f3e57708e32b93331a961a47a521ab
Ernesto Rivas <64172275+ErnestoRivas99@users.noreply.github.com>
2020-05-26 01:57:12 -0500

    Buenas madrugadas que dios lo ayude en su dia

---
+++ keys.js
@@ -0,0 +1,11 @@
+module.exports = {
+
+    database: {
+        connectionLimit: 10,
+        host: 'sql9.freemysqlhosting.net',
+        user: 'sql3342944',
+        password: 'r5DvFbQeV5',
+        database: 'sql3342944'
+    }
+
+};
